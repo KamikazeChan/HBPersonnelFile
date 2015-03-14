ad0e8784fc79944f178f41226b661cd0ffd1ea86
hefju <hefju@live.cn>
2015-03-14 08:52:21 +0800

    签入分配权限窗口

---
+++ HBPersonnelFile/SYS/FrmUserAuth.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace HBPersonnelFile.SYS
+{
+    partial class FrmUserAuth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menutjqx添加权限 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cbUserName = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.btnLoad = new DevComponents.DotNetBar.ButtonX();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.tabControl1 = new DevComponents.DotNetBar.TabControl();
+            this.tabControlPanel1 = new DevComponents.DotNetBar.TabControlPanel();
+            this.lblInfo = new DevComponents.DotNetBar.LabelX();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.treeViewUser = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuScqx删除权限 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabItem1 = new DevComponents.DotNetBar.TabItem(this.components);
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tabControl1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabControlPanel1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.ContextMenuStrip = this.contextMenuStrip1;
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold);
+            this.treeView.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.treeView.Location = new System.Drawing.Point(3, 3);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(265, 530);
+            this.treeView.TabIndex = 2;
+            this.treeView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeView_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menutjqx添加权限});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 26);
+            // 
+            // menutjqx添加权限
+            // 
+            this.menutjqx添加权限.Name = "menutjqx添加权限";
+            this.menutjqx添加权限.Size = new System.Drawing.Size(124, 22);
+            this.menutjqx添加权限.Text = "添加权限";
+            this.menutjqx添加权限.Click += new System.EventHandler(this.menutjqx添加权限_Click);
+            // 
+            // cbUserName
+            // 
+            this.cbUserName.DisplayMember = "Text";
+            this.cbUserName.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbUserName.FormattingEnabled = true;
+            this.cbUserName.Location = new System.Drawing.Point(89, 41);
+            this.cbUserName.Name = "cbUserName";
+            this.cbUserName.Size = new System.Drawing.Size(121, 22);
+            this.cbUserName.TabIndex = 3;
+            this.cbUserName.Text = "管理员";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnLoad.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnLoad.Location = new System.Drawing.Point(221, 40);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(103, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "读取用户权限";
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // labelX1
+            // 
+            this.labelX1.AutoSize = true;
+            this.labelX1.Location = new System.Drawing.Point(39, 44);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(44, 16);
+            this.labelX1.TabIndex = 5;
+            this.labelX1.Text = "用户名";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.CanReorderTabs = true;
+            this.tabControl1.Controls.Add(this.tabControlPanel1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(279, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedTabFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold);
+            this.tabControl1.SelectedTabIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(729, 562);
+            this.tabControl1.TabIndex = 6;
+            this.tabControl1.TabLayoutType = DevComponents.DotNetBar.eTabLayoutType.FixedWithNavigationBox;
+            this.tabControl1.Tabs.Add(this.tabItem1);
+            this.tabControl1.Text = "tabControl1";
+            // 
+            // tabControlPanel1
+            // 
+            this.tabControlPanel1.Controls.Add(this.lblInfo);
+            this.tabControlPanel1.Controls.Add(this.labelX4);
+            this.tabControlPanel1.Controls.Add(this.labelX3);
+            this.tabControlPanel1.Controls.Add(this.labelX2);
+            this.tabControlPanel1.Controls.Add(this.treeViewUser);
+            this.tabControlPanel1.Controls.Add(this.btnLoad);
+            this.tabControlPanel1.Controls.Add(this.labelX1);
+            this.tabControlPanel1.Controls.Add(this.cbUserName);
+            this.tabControlPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlPanel1.Location = new System.Drawing.Point(0, 26);
+            this.tabControlPanel1.Name = "tabControlPanel1";
+            this.tabControlPanel1.Padding = new System.Windows.Forms.Padding(1);
+            this.tabControlPanel1.Size = new System.Drawing.Size(729, 536);
+            this.tabControlPanel1.Style.BackColor1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(179)))), ((int)(((byte)(231)))));
+            this.tabControlPanel1.Style.BackColor2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(237)))), ((int)(((byte)(254)))));
+            this.tabControlPanel1.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine;
+            this.tabControlPanel1.Style.BorderColor.Color = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(97)))), ((int)(((byte)(156)))));
+            this.tabControlPanel1.Style.BorderSide = ((DevComponents.DotNetBar.eBorderSide)(((DevComponents.DotNetBar.eBorderSide.Left | DevComponents.DotNetBar.eBorderSide.Right) 
+            | DevComponents.DotNetBar.eBorderSide.Bottom)));
+            this.tabControlPanel1.Style.GradientAngle = 90;
+            this.tabControlPanel1.TabIndex = 1;
+            this.tabControlPanel1.TabItem = this.tabItem1;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(39, 94);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(56, 16);
+            this.lblInfo.TabIndex = 10;
+            this.lblInfo.Text = "用户权限";
+            // 
+            // labelX4
+            // 
+            this.labelX4.AutoSize = true;
+            this.labelX4.Location = new System.Drawing.Point(378, 128);
+            this.labelX4.Name = "labelX4";
+            this.labelX4.Size = new System.Drawing.Size(186, 16);
+            this.labelX4.TabIndex = 9;
+            this.labelX4.Text = "3. 右键点击用户权限, 点击删除";
+            // 
+            // labelX3
+            // 
+            this.labelX3.AutoSize = true;
+            this.labelX3.Location = new System.Drawing.Point(378, 87);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(210, 16);
+            this.labelX3.TabIndex = 8;
+            this.labelX3.Text = "2. 右键点击系统全部权限, 点击添加";
+            // 
+            // labelX2
+            // 
+            this.labelX2.AutoSize = true;
+            this.labelX2.Location = new System.Drawing.Point(378, 44);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(99, 16);
+            this.labelX2.TabIndex = 7;
+            this.labelX2.Text = "1. 读取用户权限";
+            // 
+            // treeViewUser
+            // 
+            this.treeViewUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeViewUser.ContextMenuStrip = this.contextMenuStrip2;
+            this.treeViewUser.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.treeViewUser.Location = new System.Drawing.Point(39, 114);
+            this.treeViewUser.Name = "treeViewUser";
+            this.treeViewUser.Size = new System.Drawing.Size(285, 401);
+            this.treeViewUser.TabIndex = 6;
+            this.treeViewUser.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeViewUser_MouseDown);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuScqx删除权限});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(153, 48);
+            // 
+            // menuScqx删除权限
+            // 
+            this.menuScqx删除权限.Name = "menuScqx删除权限";
+            this.menuScqx删除权限.Size = new System.Drawing.Size(152, 22);
+            this.menuScqx删除权限.Text = "删除权限";
+            this.menuScqx删除权限.Click += new System.EventHandler(this.menuScqx删除权限_Click);
+            // 
+            // tabItem1
+            // 
+            this.tabItem1.AttachedControl = this.tabControlPanel1;
+            this.tabItem1.Name = "tabItem1";
+            this.tabItem1.Text = "设置用户权限";
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage1);
+            this.tabControl2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tabControl2.Location = new System.Drawing.Point(0, 0);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(279, 562);
+            this.tabControl2.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.treeView);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(271, 536);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "系统全部权限";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // FrmUserAuth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 562);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tabControl2);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "FrmUserAuth";
+            this.Text = "设置用户权限";
+            this.Load += new System.EventHandler(this.FrmUserAuth_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tabControl1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabControlPanel1.ResumeLayout(false);
+            this.tabControlPanel1.PerformLayout();
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx cbUserName;
+        private DevComponents.DotNetBar.ButtonX btnLoad;
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.TabControl tabControl1;
+        private DevComponents.DotNetBar.TabControlPanel tabControlPanel1;
+        private DevComponents.DotNetBar.TabItem tabItem1;
+        private System.Windows.Forms.TreeView treeViewUser;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private DevComponents.DotNetBar.LabelX lblInfo;
+        private DevComponents.DotNetBar.LabelX labelX4;
+        private DevComponents.DotNetBar.LabelX labelX3;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menutjqx添加权限;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem menuScqx删除权限;
+    }
+}
---
+++ HBPersonnelFile/SYS/FrmUserAuth.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Model;
+
+namespace HBPersonnelFile.SYS
+{
+    public partial class FrmUserAuth : WeifenLuo.WinFormsUI.Docking.DockContent
+    {
+        public FrmUserAuth()
+        {
+            InitializeComponent();
+        }
+        Tyg员工 selectedUser;//当前选中的用户,分配权限就分配到该用户了.
+        TUserAuth userAuth;//选中用户的菜单权限 
+        private void FrmUserAuth_Load(object sender, EventArgs e)
+        {
+            string sql = "select * from Tcd菜单 order by TypeOrder,SOrder";
+            var dt = RemoteServer.GetTable(sql);
+
+            var lst = ToEntity(dt);
+            TreeNode parent = null;
+            foreach (var i in lst)
+            {
+                if (i.FuncName == "")
+                {
+                    TreeNode tmp = new TreeNode(i.NType);
+                    tmp.Tag = i;
+
+                    parent = tmp;
+                    treeView.Nodes.Add(tmp);
+                }
+                else
+                {
+                    TreeNode tmp = new TreeNode(i.FuncName);
+                    tmp.Tag = i;
+
+                    parent.Nodes.Add(tmp);
+                }
+            }
+
+            treeView.ExpandAll();
+        }
+        private List<Model.Tcd菜单> ToEntity(DataTable dt)
+        {
+            Model.Tcd菜单 e = new Model.Tcd菜单();
+            return e.ConvertToList(dt);
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (cbUserName.Text.Length < 1) return;
+
+            Tyg员工 i = new Tyg员工();
+            var lst=i.FindEmployeeByName(cbUserName.Text);
+            if(lst.Count!=1)
+            {
+                selectedUser = null;
+                lblInfo.Text = "用户权限";
+                MessageBox.Show("找不到员工:"+cbUserName.Text);
+                return;
+            }
+            selectedUser = lst[0];
+            TUserAuth ua = new TUserAuth();
+            var lstUa = ua.GetEntityByUserID(selectedUser.ID);
+            if(lstUa.Count<1)
+            {
+                userAuth = ua;
+                userAuth.ID = 0;
+                userAuth.UserID = selectedUser.ID;
+                userAuth.MenuIDs = "";
+            }else
+            {
+                userAuth = lstUa[0];
+            }
+
+            lblInfo.Text = selectedUser.X姓名+"权限";
+            FillUserTree(userAuth);
+        }
+
+        private void FillUserTree(TUserAuth userAuth)
+        {
+            treeViewUser.Nodes.Clear();
+            if (userAuth.MenuIDs.Length < 1) return;
+
+            var sql = string.Format("select * from Tcd菜单 WHERE ID in ({0}) order by TypeOrder,SOrder", userAuth.MenuIDs);
+            var dt = RemoteServer.GetTable(sql);
+
+            var lst = ToEntity(dt);
+            TreeNode parent = null;
+            foreach (var i in lst)
+            {
+                if (i.FuncName == "")
+                {
+                    TreeNode tmp = new TreeNode(i.NType);
+                    tmp.Tag = i;
+
+                    parent = tmp;
+                    treeViewUser.Nodes.Add(tmp);
+                }
+                else
+                {
+                    TreeNode tmp = new TreeNode(i.FuncName);
+                    tmp.Tag = i;
+
+                    parent.Nodes.Add(tmp);
+                }
+            }
+
+            treeViewUser.ExpandAll();
+        }
+
+        #region 右键选中树形控件
+        private void treeView_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                TreeNode tn = treeView.GetNodeAt(e.X, e.Y);
+                if (tn != null) treeView.SelectedNode = tn;
+            }
+        }
+
+        private void treeViewUser_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                TreeNode tn = treeViewUser.GetNodeAt(e.X, e.Y);
+                if (tn != null) treeViewUser.SelectedNode = tn;
+            }
+        }
+
+        #endregion
+
+    
+        private void menutjqx添加权限_Click(object sender, EventArgs e)
+        {
+            if(selectedUser==null)
+            {
+                MessageBox.Show("请选择用户");
+                return;
+            }
+            var node = treeView.SelectedNode;
+            List<int> ids = GetSubTree(node);
+
+            var affect =userAuth.MergeAndSave(ids);
+            if(affect>0)
+            {
+                FillUserTree(userAuth);
+            }
+            else
+            {
+                MessageBox.Show("添加权限失败.");
+            }
+        }
+
+        private   List<int> GetSubTree(TreeNode node )
+        {
+          List<int> ids=new List<int>();
+            //父节点
+            var parent=node.Parent;
+            while (parent != null)
+            {
+                Tcd菜单 cd=(Tcd菜单)parent.Tag;
+                ids.Add(cd.ID);
+                parent = parent.Parent;
+            }
+            //获取所有子节点
+            GetSubNode(node,ids);
+
+            return ids;
+        }
+
+        private void GetSubNode(TreeNode node, List<int> ids)//递归获取子节点
+        {
+            Tcd菜单 cd = (Tcd菜单)node.Tag;
+            ids.Add(cd.ID);
+            foreach (TreeNode node0 in node.Nodes)
+            {
+                GetSubNode(node0, ids);
+            }
+        }
+
+        private void menuScqx删除权限_Click(object sender, EventArgs e)
+        {
+            if (selectedUser == null)
+            {
+                MessageBox.Show("请选择用户");
+                return;
+            }
+            
+            if (treeViewUser.SelectedNode==null) return;
+            var node = treeViewUser.SelectedNode;
+            List<int> ids = new List<int>();
+            GetSubNode(node, ids);
+
+            var affect = userAuth.DeleteAndSave(ids);
+            if (affect > 0)
+            {
+                FillUserTree(userAuth);
+            }
+            else
+            {
+                MessageBox.Show("删除权限失败.");
+            }
+        }
+
+    }
+}
